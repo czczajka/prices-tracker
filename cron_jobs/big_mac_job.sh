3be857cd2030d801886d599ab2c6e8e6279cce1a
Cezary Czajka <cz.czajka@gmail.com>
2021-11-05 21:28:57 +0100

    Modify cron job to script to use it on heroku.

--- cron_jobs/big_mac_job.sh
+++ cron_jobs/big_mac_job.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
 
-echo -n `date +"\%Y-\%m-\%d,"` >>  big_mac_data.txt
-python ../scripts/big_mac_parser.py >> big_mac_data.txt
-curl http://127.0.0.1:8000/g_plots
+echo -n `date +"%Y-%m-%d,"` >>  ../data/big_mac_data.txt
+python ../scripts/big_mac_parser.py >> ../data/big_mac_data.txt
+
+echo -n `date +"%Y-%m-%d,"` >>  ../data/coca_cola_data.txt
+python ../scripts/coca_cola_parser.py >> ../data/coca_cola_data.txt
+
+curl https://price-trend.herokuapp.com/g_plots
