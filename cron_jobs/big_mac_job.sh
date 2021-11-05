ec6fb52a1ff3c207b817d643960cebd1d55eda63
Cezary Czajka <cz.czajka@gmail.com>
2021-11-05 21:44:43 +0100

    Correct cron job.

--- cron_jobs/big_mac_job.sh
+++ cron_jobs/big_mac_job.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
-echo -n `date +"%Y-%m-%d,"` >>  ../data/big_mac_data.txt
-python ../scripts/big_mac_parser.py >> ../data/big_mac_data.txt
+echo -n `date +"%Y-%m-%d,"` >>  /app/cron_jobs/data/big_mac_data.txt
+python /app/cron_jobs/scripts/big_mac_parser.py >> /app/cron_jobs/data/big_mac_data.txt
 
-echo -n `date +"%Y-%m-%d,"` >>  ../data/coca_cola_data.txt
-python ../scripts/coca_cola_parser.py >> ../data/coca_cola_data.txt
+echo -n `date +"%Y-%m-%d,"` >>  /app/cron_jobs/data/coca_cola_data.txt
+python /app/cron_jobs/scripts/coca_cola_parser.py >> /app/cron_jobs/data/coca_cola_data.txt
 
 curl https://price-trend.herokuapp.com/g_plots
